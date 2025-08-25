7326c45f9913eb5ac856093d61264c35f01232a5
김재혁 <2017387@vieworks.com>
2025-08-25 16:19:10 +0900

    Create IAM Role in SageMaker

---
+++ script/sagemaker_create_iam_role.sh
@@ -0,0 +1,26 @@
+# 1-1. Create IAM Role (Assume Policy 지정)
+aws iam create-role \
+  --role-name AmazonSageMaker-ExecutionRole \
+  --assume-role-policy-document '{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": { "Service": "sagemaker.amazonaws.com" },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }'
+
+# 1-2. Apply IAM role (ECR, S3, CloudWatch)
+aws iam attach-role-policy \
+  --role-name AmazonSageMaker-ExecutionRole \
+  --policy-arn arn:aws:iam::aws:policy/AmazonSageMakerFullAccess
+
+aws iam attach-role-policy \
+  --role-name AmazonSageMaker-ExecutionRole \
+  --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
+
+aws iam attach-role-policy \
+  --role-name AmazonSageMaker-ExecutionRole \
+  --policy-arn arn:aws:iam::aws:policy/CloudWatchFullAccess
