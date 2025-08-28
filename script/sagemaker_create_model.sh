162addfa83d49bad3c3dd1a72d5b6110d5f13a38
김재혁 <2017387@vieworks.com>
2025-08-28 15:21:57 +0900

    update for create sagemaker model

--- script/sagemaker_create_model.sh
+++ script/sagemaker_create_model.sh
@@ -9,5 +9,5 @@ echo "[INFO] Creating SageMaker Model: $DDN_MODEL_NAME"
 aws sagemaker create-model \
   --region $AWS_REGION \
   --model-name $DDN_MODEL_NAME \
-  --primary-container Image=$DDN_IMAGE_URI \
+  --primary-container "Image=$DDN_IMAGE_URI,ContainerEntrypoint=[\"sh\",\"-c\"],ContainerArguments=[\"tritonserver --model-repository=/models & python3 /opt/program/inference_handler.py & wait\"]" \
   --execution-role-arn $DDN_EXEC_ROLE_ARN
