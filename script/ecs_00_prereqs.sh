ec7162bd58e2126034160525508a4e293fe11523
김재혁 <2017387@vieworks.com>
2025-09-11 14:26:34 +0900

    update ecs prereqs

--- script/ecs_00_prereqs.sh
+++ script/ecs_00_prereqs.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 source .env
 
+sudo yum install -y gettext jq -q
+
 command -v aws >/dev/null || { echo "[ERROR] aws CLI not found"; exit 1; }
 aws sts get-caller-identity >/dev/null || { echo "[ERROR] AWS credentials not configured"; exit 1; }
 
