73780532ad220b3d9fdf48292c8c3f2776fc1d17
김재혁 <2017387@vieworks.com>
2025-09-19 16:09:35 +0900

    Refactor Lambda function packaging and add .gitignore for temporary files

--- script/lambda_01_create_function.sh
+++ script/lambda_01_create_function.sh
@@ -2,9 +2,11 @@
 set -euo pipefail
 source .env
 
+FUNC_ZIP_FILE="ddn_lambda_function.zip"
+
 echo "[INFO] Packaging Lambda function"
-rm -f ddn_lambda_function.zip
-zip ddn_lambda_function.zip lambda_function.py
+rm -f $FUNC_ZIP_FILE
+zip $FUNC_ZIP_FILE lambda_function.py
 
 echo "[INFO] Creating Lambda function: $DDN_LAMBDA_FUNC_NAME"
 aws lambda create-function \
@@ -12,5 +14,5 @@ aws lambda create-function \
   --runtime python3.12 \
   --role arn:aws:iam::$ACCOUNT_ID:role/$DDN_LAMBDA_ROLE \
   --handler lambda_function.lambda_handler \
-  --zip-file fileb://ddn_lambda_function.zip \
+  --zip-file fileb://$FUNC_ZIP_FILE \
   --environment "Variables={DDN_IN_BUCKET=$DDN_IN_BUCKET,DDN_OUT_BUCKET=$DDN_OUT_BUCKET}"
