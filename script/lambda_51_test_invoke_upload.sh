28ce9b4db6ee1f9a98b6bb102e57207cc0cd53d5
김재혁 <2017387@vieworks.com>
2025-09-19 15:16:03 +0900

    Refactor Lambda upload and download scripts for improved clarity and functionality

--- script/lambda_51_test_invoke_upload.sh
+++ script/lambda_51_test_invoke_upload.sh
@@ -10,15 +10,22 @@ echo "[INFO] Target S3 key: $DDN_TEST_IMAGE_KEY"
 echo "[INFO] Requesting presigned URL for upload..."
 aws lambda invoke \
   --function-name $DDN_LAMBDA_FUNC_NAME \
-  --payload "{\"queryStringParameters\":{\"mode\":\"upload\",\"file\":\"$DDN_TEST_IMAGE_KEY\"}}" \
+  --payload "{\"queryStringParameters\":{\"mode\":\"upload\",\"file\":\"$IMAGE_KEY\"}}" \
   upload_response.json \
   --region $AWS_REGION \
   --cli-binary-format raw-in-base64-out >/dev/null
 
-UPLOAD_URL=$(jq -r '.url' upload_response.json)
+# echo upload_response.json
+cat upload_response.json
+
+# Parse Upload URL
+UPLOAD_URL=$(jq -r '.body' upload_response.json | jq -r '.url')
 echo "[INFO] Upload URL: $UPLOAD_URL"
 
 # 2. Upload Image
 echo "[INFO] Uploading image to S3 via presigned URL..."
 STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X PUT --upload-file "$DDN_TEST_IMAGE_PATH" "$UPLOAD_URL")
 echo "[INFO] Upload finished with HTTP status: $STATUS"
+
+rm -f upload_response.json
+echo "[INFO] Test finished."
--- script/lambda_52_test_invoke_download.sh.sh
+++ script/lambda_52_test_invoke_download.sh.sh
@@ -5,17 +5,16 @@ source .env
 echo "[INFO] Testing Lambda function: $DDN_LAMBDA_FUNC_NAME (download)"
 echo "[INFO] Target S3 key: $DDN_TEST_IMAGE_KEY"
 
-# 1. Request Presigned
-echo "[INFO] Requesting presigned URL for download..."
-aws lambda invoke \
-  --function-name $DDN_LAMBDA_FUNC_NAME \
-  --payload "{\"queryStringParameters\":{\"mode\":\"download\",\"file\":\"$IMAGE_KEY="user/static_demo_140um_madible_VD.tif"
-\"}}" \
-  download_response.json \
-  --region $AWS_REGION \
-  --cli-binary-format raw-in-base64-out >/dev/null
+# 1. Request Presigned (파이프 방식, 임시파일 없음)
+DOWNLOAD_URL=$(
+  aws lambda invoke \
+    --function-name $DDN_LAMBDA_FUNC_NAME \
+    --payload "{\"queryStringParameters\":{\"mode\":\"download\",\"file\":\"$DDN_TEST_IMAGE_KEY\"}}" \
+    --region $AWS_REGION \
+    --cli-binary-format raw-in-base64-out \
+    /dev/stdout | jq -r '.body' | jq -r '.url'
+)
 
-DOWNLOAD_URL=$(jq -r '.url' download_response.json)
 echo "[INFO] Download URL: $DOWNLOAD_URL"
 
 # 2. Download
